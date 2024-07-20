91dbb19c6333571eefbf36895fb08bbc643a8d8f
Atechchatterjee <atechchatterjeee@gmail.com>
2024-07-20 18:27:21 +0530

    feat: build out the backend to handle reads and writes for blogs

--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,50 @@
 import express from "express";
 import cors from "cors";
+import { blog, getCurrentTimeIST } from "./lib/utils";
+import { ObjectId } from "mongodb";
 
 const app = express();
 const port = 8000;
 
 app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is running :)");
+});
+
+app.get("/fetch-all-blogs", async (req, res) => {
+  const blogs = await blog.find().toArray();
+  console.log({ blogs });
+  res.send(JSON.stringify(blogs));
+});
+
+app.get("/fetch-blog", async (req, res) => {
+  console.log("query id: ", req.query.id);
+  if (typeof req.query.id !== "string") return res.status(400);
+  const id = new ObjectId(req.query.id);
+  const requestedBlog = await blog.findOne({ _id: id });
+  console.log(requestedBlog);
+  res.send(JSON.stringify(requestedBlog));
+});
+
+app.post("/create-blogpost", async (req, res) => {
+  console.log("inserting = ", req.body);
+  try {
+    const out = await blog.insertOne({
+      title: req.body.title,
+      description: req.body.description,
+      dateTime: getCurrentTimeIST(),
+      likes: 0,
+    });
+    console.log(out);
+    res.send("OK");
+  } catch (err) {
+    console.error(err);
+    return res.status(500);
+  }
 });
---
+++ backend/lib/utils.ts
@@ -0,0 +1,18 @@
+import { MongoClient } from "mongodb";
+
+export const mongoClient = new MongoClient(process.env.MONGO_URI ?? "");
+
+export const database = mongoClient.db("frokerblog");
+export const blog = database.collection("blog");
+
+export function getCurrentTimeIST() {
+  const date = new Date();
+  let normalizedDateTime = new Date(
+    date.getTime() +
+      (date.getTimezoneOffset() == 0 ? 0 : -1 * date.getTimezoneOffset()) *
+        60000
+  );
+  return new Date(
+    normalizedDateTime.toLocaleString("en-US", { timeZone: "Asia/Calcutta" })
+  );
+}
